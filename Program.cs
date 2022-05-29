a346babbf0f49999d72b6835f40cf25b2abb10d4
ibrahimsarikaya <ibo7534@gmail.com>
2022-05-29 23:48:34 +0300

    Codding

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace if_elseif // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time<11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+
+            else if(time <= 18)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else
+                Console.WriteLine("İyi geceler");
+
+            string sonuc = time<=18 ? "İyi Günler" : "İyi Geceler";
+
+            sonuc = time>=6 && time<11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+
         }
     }
 }
